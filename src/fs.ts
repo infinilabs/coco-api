a31a27a433aac4d211e9eeadd9b5b31fc39920d0
Steve Lau <stevelauc@outlook.com>
2025-09-09 16:02:06 +0800

    format code

--- src/fs.ts
+++ src/fs.ts
@@ -1,31 +1,52 @@
 import type * as nodejs_fs from 'fs';
 
-type ReaddirCallback = (err: NodeJS.ErrnoException | null, files: string[]) => void;
-type ReaddirWithFileTypesCallback = (err: NodeJS.ErrnoException | null, files: nodejs_fs.Dirent[]) => void;
-
 /**
  * Asynchronously reads the contents of a directory.
+ *
  * @param path A path to a file. If a URL is provided, it must use the `file:` protocol.
- * @param callback The callback function.
+ *
+ * @param options The encoding (or an object specifying the encoding), used as the
+ * encoding of the result. If `withFileTypes` is set to `true`, the result will contain `fs.Dirent` objects.
  */
-export function readdir(path: nodejs_fs.PathLike, callback: ReaddirCallback): void;
+export function readdir(
+    path: nodejs_fs.PathLike,
+    options?:
+        | { encoding: BufferEncoding | null; withFileTypes?: false }
+        | BufferEncoding
+        | null,
+): Promise<string[]>;
 
 /**
  * Asynchronously reads the contents of a directory.
+ *
  * @param path A path to a file. If a URL is provided, it must use the `file:` protocol.
- * @param options The encoding (or an object specifying the encoding), used as the encoding of the result. If `withFileTypes` is set to `true`, the result will contain `fs.Dirent` objects.
- * @param callback The callback function.
- */
-export function readdir(path: nodejs_fs.PathLike, options: { encoding: BufferEncoding | null; withFileTypes?: false } | BufferEncoding | undefined | null, callback: ReaddirCallback): void;
-
-/**
- * Asynchronously reads the contents of a directory.
- * @param path A path to a file. If a URL is provided, it must use the `file:` protocol.
+ *
  * @param options If `withFileTypes` is set to `true`, the result will contain `fs.Dirent` objects.
- * @param callback The callback function.
  */
-export function readdir(path: nodejs_fs.PathLike, options: { encoding?: BufferEncoding | null; withFileTypes: true }, callback: ReaddirWithFileTypesCallback): void;
+export function readdir(
+    path: nodejs_fs.PathLike,
+    options: { encoding?: BufferEncoding | null; withFileTypes: true },
+): Promise<nodejs_fs.Dirent[]>;
 
-export function readdir(path: nodejs_fs.PathLike, options: any, callback?: any): void {
-  throw new Error("Not implemented");
+/**
+ * Asynchronously reads the contents of a directory.
+ *
+ * @param path A path to a file. If a URL is provided, it must use the `file:` protocol.
+ *
+ * @param options The encoding (or an object specifying the encoding), used as the
+ * encoding of the result. If `withFileTypes` is set to `true`, the result will contain `fs.Dirent` objects.
+ */
+export function readdir(
+    path: nodejs_fs.PathLike,
+    options?:
+        | { encoding?: BufferEncoding | null; withFileTypes?: boolean }
+        | BufferEncoding
+        | null,
+): Promise<string[] | nodejs_fs.Dirent[]>;
+
+export function readdir(
+    path: nodejs_fs.PathLike,
+    options?: any,
+): Promise<string[] | nodejs_fs.Dirent[]> {
+    throw new Error('Not implemented');
 }
